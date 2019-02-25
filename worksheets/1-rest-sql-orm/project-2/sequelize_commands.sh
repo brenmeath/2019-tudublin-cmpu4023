826a584edf39414603c9fb0c0dc1c3c903c59116
Brendan Meath <bren.meath@gmail.com>
2019-02-25 09:24:28 +0000

    Remove wks-1 files from wks-2 branch

--- worksheets/1-rest-sql-orm/project-2/sequelize_commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sequelize model:create --name User --attributes id:integer,email:string,password:string,details:string,created_at:date,deleted_at:date
-sequelize model:create --name Product --attributes id:integer,title:string,price:numeric,created_at:date,deleted_at:date,tags:string
-sequelize model:create --name PurchaseItem --attributes id:integer,purchaseId:integer,productId:integer,price:float,quantity:integer,state:string
-sequelize model:create --name Purchase --attributes id:integer,name:string,address:string,state:string,zipcode:integer,userId:integer
-
-sequelize seed:create --name user-seeder
-sequelize seed:create --name product-seeder
